f20ca66f25baa74dc3774c6b61808c13e39fba5f
Adriel Rodrigues <120425687+NokFromDFuture@users.noreply.github.com>
2023-10-02 21:25:42 -0300

    Create lab1.rb
    Pasta com atividades do curso Ruby developer da DIO

---
+++ DIO - Ruby Dev/lab1.rb
@@ -0,0 +1,10 @@
+print "Bem vindo a Biblioteca Online..Digite seu nome: "
+name = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá aluno #{name} #{sobrenome}, #{idade}."
